d7941ba493c260711b8300b7a71e31ea4f002849
Lusako <100602021+Lusako@users.noreply.github.com>
2024-09-20 09:25:13 +0200

    Update run_BWA_mem_1.sh

--- scripts/minority_variants/run_BWA_mem_1.sh
+++ scripts/minority_variants/run_BWA_mem_1.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Define your arrays
-obams=("45907_2#336.bam" "45907_2#293.bam" "45897_1#42.bam" "45897_2#220.bam" "45897_1#211.bam" "45907_2#306.bam" "45907_2#14.bam" "45907_2#100.bam" "45907_1#169.bam" "45907_2#91.bam" "45907_2#155.bam" "45907_1#195.bam" "45907_2#174.bam")
-reffas=("GCA_015839035.1_ASM1583903v1_genomic.fna" "GCA_015839035.1_ASM1583903v1_genomic.fna" "GCA_015839035.1_ASM1583903v1_genomic.fna" "GCA_015839035.1_ASM1583903v1_genomic.fna" "GCA_015839035.1_ASM1583903v1_genomic.fna" "GCA_015839035.1_ASM1583903v1_genomic.fna" "GCA_015839035.1_ASM1583903v1_genomic.fna" "GCA_015839035.1_ASM1583903v1_genomic.fna" "GCA_015839035.1_ASM1583903v1_genomic.fna" "GCA_015839035.1_ASM1583903v1_genomic.fna" "GCA_015839035.1_ASM1583903v1_genomic.fna" "GCA_015839035.1_ASM1583903v1_genomic.fna" "GCA_015839035.1_ASM1583903v1_genomic.fna")
-fq1s=("45907_2#336_1.fastq.gz" "45907_2#293_1.fastq.gz" "45897_1#42_1.fastq.gz" "45897_2#220_1.fastq.gz" "45897_1#211_1.fastq.gz" "45907_2#306_1.fastq.gz" "45907_2#14_1.fastq.gz" "45907_2#100_1.fastq.gz" "45907_1#169_1.fastq.gz" "45907_2#91_1.fastq.gz" "45907_2#155_1.fastq.gz" "45907_1#195_1.fastq.gz" "45907_2#174_1.fastq.gz")
-fq2s=("45907_2#336_2.fastq.gz" "45907_2#293_2.fastq.gz" "45897_1#42_2.fastq.gz" "45897_2#220_2.fastq.gz" "45897_1#211_2.fastq.gz" "45907_2#306_2.fastq.gz" "45907_2#14_2.fastq.gz" "45907_2#100_2.fastq.gz" "45907_1#169_2.fastq.gz" "45907_2#91_2.fastq.gz" "45907_2#155_2.fastq.gz" "45907_1#195_2.fastq.gz" "45907_2#174_2.fastq.gz")
+obams=( "bam_file")
+reffas=("GCA_015839035.1_ASM1583903v1_genomic.fna")
+fq1s=("1.fastq_file")
+fq2s=("2.fastq_file")
 
 # Loop through each set of files
 for i in "${!obams[@]}"; do
